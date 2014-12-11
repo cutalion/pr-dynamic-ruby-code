919977576490a978ff476e618db27cfea51a5ec2
Alexander Glushkov <cutalion@gmail.com>
2014-12-11 17:56:58 +0300

    Add slide sources

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  theme = require('bespoke-theme-voltaire'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
+
+// Bespoke.js
+bespoke.from('article', [
+  theme(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+  forms()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+require('prism/components/prism-ruby');
+require('prism/components/prism-bash');
+require('prism/plugins/line-numbers/prism-line-numbers');
+
